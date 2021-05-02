a3c75943f26895f6b3273b0494c81321cd2b23f4
brayan <brayanjaviergomez.g@gmail.com>
2021-05-01 20:33:13 -0500

    Covid 19 map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -14,6 +14,16 @@ import { HttpClient } from '@angular/common/http';
 
 export class MapComponent implements OnInit {
 
+  data_us = {
+    confirmados: 'No confirmado',
+
+    muertos: 'No confirmado',
+
+    recuperados: 'No confirmado',
+
+    total: 'No confirmado'
+  };
+
   side = false;
 
   cargando;
@@ -77,9 +87,23 @@ export class MapComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+
+
     $('#menu-toggle').click(function (e) {
       e.preventDefault();
       $("#wrapper").toggleClass('toggled');
+    });
+
+
+
+    this.http.get('https://www.trackcorona.live/api/countries/us').subscribe((data: any) => {
+      console.log(data);
+      this.data_us.confirmados = data.data[0].confirmed;
+      this.data_us.recuperados = data.data[0].recovered;
+      this.data_us.muertos = data.data[0].dead;
+      this.data_us.total = (data.data[0].confirmed + data.data[0].recovered + data.data[0].dead)
+      console.log(this.data_us);
+
     });
   }
 
@@ -99,53 +123,79 @@ export class MapComponent implements OnInit {
 
     ).subscribe((data: any) => {
 
-      if (data.features[0].text){
+      if (data.features[0].text) {
         this.nombre_estado = encodeURI(data.features[0].text);
       }
 
-
       console.log(this.nombre_estado);
 
-      this.http.get(`${this.url_corona}/${this.nombre_estado}`).subscribe((datos: any) => {
-
-        this.cargando = false;
-
-        console.log(datos);
-
-        if (datos.data.length === 0) {
-
-          this.data.location = 'No es posible obtener datos de la localizacion';
-
-          this.data.confirmados = 'N/A';
-
-          this.data.muertos = 'N/A';
-
-          this.data.recuperados = 'N/A';
-
-          return console.log('no esta disponible');
-
-        }
-
-
-
-        this.data.confirmados = datos.data[0].confirmed;
-
-        this.data.muertos = datos.data[0].dead;
-
-
-        if (!datos.data[0].recovered === null) {
-
-          this.data.recuperados = datos.data[0].recovered;
-
-        }
-
-        this.data.location = datos.data[0].location;
-
-      });
+      this.get_covid();
 
     });
 
     console.log(this.data);
 
   }
+
+  get_covid() {
+    this.http.get(`${this.url_corona}/${this.nombre_estado}`).subscribe((datos: any) => {
+
+      this.cargando = false;
+
+      console.log(datos);
+
+      if (datos.data.length === 0) {
+
+        this.data.location = 'No es posible obtener datos de la localizacion';
+
+        this.data.confirmados = 'N/A';
+
+        this.data.muertos = 'N/A';
+
+        this.data.recuperados = 'N/A';
+
+        return console.log('no esta disponible');
+
+      }
+
+
+
+      this.data.confirmados = datos.data[0].confirmed;
+
+      this.data.muertos = datos.data[0].dead;
+
+
+      if (!datos.data[0].recovered === null) {
+
+        this.data.recuperados = datos.data[0].recovered;
+
+      }
+
+      this.data.location = datos.data[0].location;
+
+    });
+  }
+
+  get_place(lugar) {
+    
+    console.log(lugar);
+
+    if (lugar === '') {
+      return;
+    }
+    this.nombre_estado = encodeURI(lugar)
+    this.http.get(
+      `${this.url_mapbox}${this.nombre_estado}.json?country=us&types=region&access_token=${this.api_key}`).subscribe((data: any) => {
+      console.log(data.features[0].center[0]);
+      this.coordenadas.coords.lat = data.features[0].center[1];
+      this.coordenadas.coords.lng = data.features[0].center[0];
+
+      this.get_covid();
+
+
+    })
+  }
+
+
+
 }

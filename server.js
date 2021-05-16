0fd63668725a8d03d79575eea926e1e13ebe33b6
brayan <brayanjaviergomez.g@gmail.com>
2021-05-15 19:05:08 -0500

    Server changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+require('./config');
+app.use(express.static(__dirname + '/dist/covid-map'));
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname + '/dist/covid-map/index.html'))
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening server in port ${process.env.PORT}`);
+})
